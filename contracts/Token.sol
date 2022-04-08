6bb63f39418e2d0df914dc5bc80359baa7054a7f
Rafał Kiełbus <rav3n.pl@gmail.com>
2022-04-08 12:21:28 +0200

    Add full token code

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,7 +1,45 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
 
-contract Token {
-  constructor() public {
-  }
+pragma solidity ^0.8.10;
+
+import "./ERC20.sol";
+import "./ERC2612.sol";
+import "./ERC3009.sol";
+import "./Recover.sol";
+
+/**
+    ERC20 token contract for LeandroLopes project
+ */
+contract LLT is ERC20, ERC2612, ERC3009, Recover {
+    //
+    // constructor
+    //
+
+    /**
+        Contract constructor
+        @param tokenName token name
+        @param tokenSymbol token symbol
+        @param contractVersion contract version
+        @param supply token supply minted to deployer
+     */
+    constructor(
+        string memory tokenName,
+        string memory tokenSymbol,
+        string memory contractVersion,
+        uint256 supply
+    ) ERC20(tokenName, tokenSymbol) {
+        // generate domainSeparator for EIP712
+        uint256 chainId = block.chainid;
+        DOMAIN_SEPARATOR = EIP712.makeDomainSeparator(
+            tokenName,
+            contractVersion,
+            chainId
+        );
+        CHAINID = chainId;
+        EIP712_DOMAIN_TYPEHASH = EIP712.EIP712_DOMAIN_TYPEHASH;
+        // mint tokens
+        _balances[msg.sender] = supply;
+        _supply = supply;
+        emit Transfer(ZERO_ADDRESS, msg.sender, supply);
+    }
 }
